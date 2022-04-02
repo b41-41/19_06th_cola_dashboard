b033a247149eef0ebeef62b12ba8ba17c2e8dfdc
DABIN JEONG <41@b41.kr>
2022-04-03 01:40:05 +0900

    Refector - Filter 메소드를 사용해서 필터링

--- src/utils/filterSearch/filterSearch.ts
+++ src/utils/filterSearch/filterSearch.ts
@@ -1,12 +1,21 @@
 import filterSearchProps from './filterSearch.type';
 
 const filterSearch = ({ inputValue = '', datas }: filterSearchProps) => {
-  const result: filterSearchProps['result'] = [];
-  datas.map(([key,value], index) => {
-    if (value.includes(inputValue)) {
-      result.push({ id: index, key:key});
-    }
+  //데이터 배열에 순서 번호 추가 (id)
+  const plusDataNumber = datas.map((item, index) => {
+    return { id: index, key: item[0], item: item[1] };
   });
+
+  //item기준으로 key값 필터링
+  const filter = plusDataNumber.filter(data => {
+    return data.item.indexOf(inputValue) !== -1;
+  });
+
+  //필터링된 값을 다시 id, key 형태로 변환 후 반환
+  const result: filterSearchProps['result'] = filter.map(item => {
+    return { id: item.id, key: item.key };
+  });
+
   return result;
 };
 
